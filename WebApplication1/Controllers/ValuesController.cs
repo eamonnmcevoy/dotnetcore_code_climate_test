9c90b2ef907e3dc73dc082cc61f416bac87de0b8
Eamonn McEvoy <emcevoy@Eamonns-MacBook-Pro.local>
2019-04-03 23:40:17 +0200

    init

---
+++ WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public static string _publicStaticString1;
+        public static string _publicStaticString2 = "";
+        
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var unusedVar = "blah";
+            return new string[] { "value1", "value2", "value3" };
+            if(false)
+                var x = 1;
+        }
+
+        public static void UnusedFunction() {
+
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
